0aabf9fbd4caf826fb4ac10b6b405749e792dd4b
Pierre Lancien <pierre@toxicode.fr>
2013-08-24 13:48:11 +0200

    added Howler lib made a minimalist event system with an eventBus

---
+++ scripts/add_event_capabilities.js
@@ -0,0 +1,35 @@
+define(function() {
+    
+	var addEventCapabilities = function (object) {
+	    
+	    object.listenersFor = {};
+	    
+	    object.on = function (eventName, callback) {
+	        if (!object.listenersFor[eventName]) {
+	            object.listenersFor[eventName] = [];
+	        }
+	        object.listenersFor[eventName].push(callback);
+	    };
+	    
+	    object.emit = function () {
+	        var args = Array.prototype.slice.call(arguments);
+	        var eventName = args.shift();
+	        var listeners = object.listenersFor[eventName] || [];
+	        
+	        for (var i=0; i < listeners.length; i++) {
+	            try {
+	                listeners[i].apply(object, args);
+	            } catch (e) {
+	               console.error('Error on event '+eventName);
+	               throw(e);
+	            }
+	        };
+	    };
+	    
+	};
+	
+
+	return addEventCapabilities;
+	
+});
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,13 @@
 require.config({
     paths: {
         'jquery':       'ext_libs/jquery',
-        'underscore':   'ext_libs/underscore'
+        'underscore':   'ext_libs/underscore',
+        'Howler':       'ext_libs/howler'
     },
     shim: {
+        'howddler': {
+            exports: 'Howl'
+        },
         'jquery': {
             exports: '$'
         },
@@ -14,10 +18,13 @@ require.config({
     urlArgs: "bust=" +  (new Date()).getTime()
 });
 
-require(['jquery', 'game'], function ($, game) {
+require(['jquery', 'game', 'add_event_capabilities'], function ($, game, addEventCapabilities) {
+    
+    var eventBus = {};
+    addEventCapabilities(eventBus);
     
     $(function() {
-        game.init($('#game'));
+        game.init($('#game'), eventBus);
     });
     
 });
